6ab2001bfd5fc56d9e12cf9a00b2fe89f6a81b02
renx <413038044@qq.com>
2021-05-20 15:36:03 +0800

    1

--- src/js/page-pre.js
+++ src/js/page-pre.js
@@ -164,3 +164,10 @@ function statisticsexps(){
         localStorage.setItem('lastAddDate',nowdate)
       }
 }
+
+
+$ajaxCache.config({ 
+  cacheValidate: function (res) {
+    //选填，配置全局的验证是否需要进行缓存的方法,“全局配置” 和 ”自定义“，至少有一处实现cacheValidate方法 return res.state === 'ok';
+    return res.code == 0;
+}})
